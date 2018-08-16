b299cc188393c64e40784223c7bc7062dbe0a5ea
Jaehee Ko <celestarry@gmail.com>
2018-08-16 11:24:11 +0900

    Add Mopub SDK

---
+++ MopubAdapterSample/MopubAdapterSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MopubAdapterSample-Bridging-Header.h
+//  MopubAdapterSample
+//
+//  Created by Jaehee Ko on 16/08/2018.
+//  Copyright © 2018 Buzzvil. All rights reserved.
+//
+
+#ifndef MopubAdapterSample_Bridging_Header_h
+#define MopubAdapterSample_Bridging_Header_h
+
+#import "MoPub-Bridging-Header.h"
+
+#endif /* MopubAdapterSample_Bridging_Header_h */
--- MopubAdapterSample/ViewController.swift
+++ MopubAdapterSample/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBOutlet var tableView: UITableView!
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+  }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
 
-
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ContentCell", for: indexPath)
+    cell.textLabel?.text = "\(indexPath.row)"
+    return cell
+  }
 }
 
